28a0a3e409d6e21813485780193a8af32fdaacb3
BandaruGirisha <71429393+BandaruGirisha@users.noreply.github.com>
2020-09-26 20:09:48 +0530

    Add files via upload

---
+++ fun prime.py
@@ -0,0 +1,9 @@
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+                print(n,'is not a prime number')
+                break
+    else:
+        print(n,'is a prime number')
+prime(19)            
+                
