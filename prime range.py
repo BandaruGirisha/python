113b8bedfaed03fe2b06df084954d8ff21e5bd85
BandaruGirisha <71429393+BandaruGirisha@users.noreply.github.com>
2020-09-26 20:18:47 +0530

    Add files via upload

---
+++ prime range.py
@@ -0,0 +1,9 @@
+a=int(input('enter a starting range'))
+b=int(input('enter a ending range'))
+for j in range(a,b+1):
+    for i in range(2,j):
+        if j%i==0:
+            break
+    else:
+        print(j,end=" ")
+               
