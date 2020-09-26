21744500211b661fa8cfe02b40de4e6ad563a1d9
BandaruGirisha <71429393+BandaruGirisha@users.noreply.github.com>
2020-09-26 20:16:48 +0530

    Add files via upload

---
+++ wperfect.py
@@ -0,0 +1,15 @@
+n=int(input('enter a number'))
+s=0
+i=1
+while i<n:
+    if n%i==0:
+         s+=i
+    i+=1     
+if n==s:
+    print(n,'is a perfect number')
+else:
+    print(n,'is not a perfect number') 
+        
+           
+        
+    
