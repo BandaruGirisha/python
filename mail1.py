85a6f899946857b98c398074fd5fa921c637a095
BandaruGirisha <71429393+BandaruGirisha@users.noreply.github.com>
2020-09-27 10:20:55 +0530

    Add files via upload

---
+++ mail1.py
@@ -0,0 +1,9 @@
+mid='thub@gmail.com'
+ipw='1234'
+umid=input('enter a mail')
+upwd=input('enter a password')
+if mid==umid and ipw==upwd:
+   print('login')
+else:
+   print('wrong credentials')
+               
